bd1c19f4193d40bfa63757c79b290ba9a4470fd9
joheborg <joheborg@gmail.com>
2024-09-12 22:55:27 -0300

    refatorado - melhorado a api rest

--- Perguntas-Gerais/Program.cs
+++ Perguntas-Gerais/Program.cs
@@ -1,21 +1,18 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MongoDB.Driver;
 using Mvc_MongoDB.Models;
-using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(
-        builder =>
-        {
-            builder.AllowAnyOrigin()
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
 });
 
 // Registre IMongoClient no contêiner de injeção de dependência
@@ -32,32 +29,40 @@ app.UseCors();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/gravarresposta/{idpergunta}/{resposta}/{correcao}", async (PerguntaService perguntaService, string idpergunta, string resposta, int correcao) =>
-{
-    var novaPergunta = new Perguntas { idpergunta = idpergunta, resposta = resposta, correcao = correcao };
-    var perguntaCriada = await perguntaService.InsertRespostaAsync(novaPergunta);
+app.MapPost(
+    "/resposta",
+    async (PerguntaService perguntaService, [FromBody] Perguntas novaPergunta) =>
+    {
+        var perguntaCriada = await perguntaService.InsertRespostaAsync(novaPergunta);
 
-    return $"Respostas gravadas com sucesso: {perguntaCriada.idpergunta}, {perguntaCriada.resposta}";
-});
-app.MapGet("/perguntas", (NovaPerguntaService novaPerguntaService) =>
-{
-    return novaPerguntaService.FindPerguntas();
-});
+        return Results.Ok(
+            $"Respostas gravadas com sucesso: {perguntaCriada.idpergunta}, {perguntaCriada.resposta}"
+        );
+    }
+);
 
-app.MapPost("/gravarnovapergunta", (NovaPerguntaService novaPerguntaService, gravarNovaPergunta novaPerguntanew) =>
-{
-    return novaPerguntaService.CreatePerguntaNova(novaPerguntanew);
-});
+app.MapGet(
+    "/perguntas",
+    (NovaPerguntaService novaPerguntaService) =>
+    {
+        return novaPerguntaService.FindPerguntas();
+    }
+);
 
+app.MapPost(
+    "/pergunta",
+    (NovaPerguntaService novaPerguntaService, gravarNovaPergunta novaPerguntanew) =>
+    {
+        return novaPerguntaService.CreatePerguntaNova(novaPerguntanew);
+    }
+);
 
-app.MapGet("/verificarrespostas", (PerguntaService perguntaService) =>
-{
-    return perguntaService.GetSum();
-});
-
+app.MapGet(
+    "/respostas",
+    (PerguntaService perguntaService) =>
+    {
+        return perguntaService.GetSum();
+    }
+);
 
 app.Run();
-
-
-
-
